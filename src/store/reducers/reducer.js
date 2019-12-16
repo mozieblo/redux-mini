a518e2f16803fdd071b6a38d02b47668140c1c91
mozieblo <m.ozieblo@wp.pl>
2019-12-16 10:59:43 +0100

    lean reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -5,18 +5,26 @@ const initialState = {
     persons: []
 }
 
+const addPerson = ( state, action ) => {
+    const newPerson = {
+        id: Math.random(),
+        name: action.personData.name,
+        age: action.personData.age /*Math.floor( Math.random() * 40 )*/
+    };
+    return updateObject(state, {persons: state.persons.concat(newPerson)});
+}
+
+const deletePerson = ( state, action ) => {
+    const updatedArray = state.persons.filter(person => person.id !== action.id);
+    return updateObject(state, {persons: updatedArray});
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_PERSON:
-            const newPerson = {
-                id: Math.random(),
-                name: action.personData.name,
-                age: action.personData.age /*Math.floor( Math.random() * 40 )*/
-            };
-            return updateObject(state, {persons: state.persons.concat(newPerson)});
+            return addPerson(state, action);
         case actionTypes.DELETE_PERSON:
-            const updatedArray = state.persons.filter(person => person.id !== action.id);
-            return updateObject(state, {persons: updatedArray});
+            return deletePerson(state, action);
         default:
             return state;
     }
